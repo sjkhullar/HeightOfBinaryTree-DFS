273b5c9d9afbbcef8d2b939e10ca553c97360fe0
sjkhullar <lovetoyogesh@gmail.com>
2015-11-26 23:16:41 +0530

    Update HeightOfTree.java

--- HeightOfBinaryTree(DFS)/src/main/java/org/learn/Question/HeightOfTree.java
+++ HeightOfBinaryTree(DFS)/src/main/java/org/learn/Question/HeightOfTree.java
@@ -7,7 +7,7 @@ public class HeightOfTree {
 		if(null == root)
 			return 0;
 		int hLeftSub = heightOfTree(root.left);
-		int hRightSub = heightOfTree(root.left);
+		int hRightSub = heightOfTree(root.right);
 		return Math.max(hLeftSub,hRightSub) + 1;
 	}
 }
